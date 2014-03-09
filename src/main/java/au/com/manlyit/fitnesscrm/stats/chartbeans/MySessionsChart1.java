b01a45a1f0e88919a0a2d561901c77677a4c23f5
david <david@david-h8-1210a>
2014-03-10 07:37:30 +1100

    merged branches finished. debug after merge

--- src/main/java/au/com/manlyit/fitnesscrm/stats/chartbeans/MySessionsChart1.java
+++ src/main/java/au/com/manlyit/fitnesscrm/stats/chartbeans/MySessionsChart1.java
@@ -7,10 +7,11 @@ package au.com.manlyit.fitnesscrm.stats.chartbeans;
 import au.com.manlyit.fitnesscrm.stats.classes.CustomersController;
 import au.com.manlyit.fitnesscrm.stats.classes.util.JsfUtil;
 import au.com.manlyit.fitnesscrm.stats.db.Customers;
+import au.com.manlyit.fitnesscrm.stats.db.SessionHistory;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.faces.context.FacesContext;
-import  org.primefaces.model.chart.LineChartSeries;
+import org.primefaces.model.chart.LineChartSeries;
 import org.primefaces.model.chart.CartesianChartModel;
 
 /**
@@ -24,6 +25,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 import javax.el.ELException;
 
 @Named("mySessionsChart1")
@@ -33,7 +35,9 @@ public class MySessionsChart1 implements Serializable {
     private CartesianChartModel model;
     private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
 
-    /** Creates a new instance of MeasurementsChart */
+    /**
+     * Creates a new instance of MeasurementsChart
+     */
     public MySessionsChart1() {
     }
     @Inject
@@ -43,7 +47,7 @@ public class MySessionsChart1 implements Serializable {
     @Inject
     private au.com.manlyit.fitnesscrm.stats.beans.SessionHistoryFacade ejbSessionHistoryFacade;
 
-    @PostConstruct
+    //@PostConstruct
     public void createChart() {
         FacesContext context = FacesContext.getCurrentInstance();
         Customers loggedInUser = null;
@@ -81,12 +85,65 @@ public class MySessionsChart1 implements Serializable {
         model = new CartesianChartModel();
         model.addSeries(groupSessionSeries);
         model.addSeries(ptSessionSeries);
-        
+
+    }
+
+    private void createSessionsChart() {
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        Customers loggedInUser = null;
+        try {
+            //loggedInUser = ejbCustomerFacade.findCustomerByUsername(context.getExternalContext().getRemoteUser());
+
+            CustomersController custController = (CustomersController) context.getApplication().evaluateExpressionGet(context, "#{customersController}", CustomersController.class);
+            loggedInUser = custController.getSelected();
+
+        } catch (ELException e) {
+            JsfUtil.addErrorMessage(e, "My Sessions Chart Critical Error", "Couldn't find the customer in the database.");
+        }
+        LineChartSeries groupSessionSeries = new LineChartSeries();
+        groupSessionSeries.setLabel("Group Sessions");
+        LineChartSeries ptSessionSeries = new LineChartSeries();
+        ptSessionSeries.setLabel("PT Sessions");
+        int weeksToDisplay = 26;
+        GregorianCalendar cal = new GregorianCalendar();
+        List<SessionHistory> sessions = null;//ejbSessionHistoryFacade.findSessionsByTrainerAndDateRange(loggedInUser.getId(), top, bottom, ptSessionIDs);
+        try {
+            for (int x = 0; x < weeksToDisplay; x++) {
+                Date top = cal.getTime();
+                cal.add(Calendar.WEEK_OF_YEAR, -1);
+                Date bottom = cal.getTime();
+                String xAxixValue = sdf.format(top);
+                sessions = ejbSessionHistoryFacade.findSessionsByTrainerAndDateRange(loggedInUser, top, bottom, true);
+                int ptCount = 0;
+                int groupCount = 0;
+                for (SessionHistory sess : sessions) {
+                    switch (sess.getId()) {
+                        case 1:// PT Sessions
+                        case 2:
+                        case 8:
+                            ptCount++;
+                            break;
+                        default:// other types
+                            groupCount++;
+                            break;
+                    }
+                }
+                ptSessionSeries.set(xAxixValue, ptCount);
+                groupSessionSeries.set(xAxixValue, groupCount);
+            }
+        } catch (Exception e) {
+            JsfUtil.addErrorMessage(e, "My Sessions Chart Critical Error", "Couldn't get customer session data from the database.");
+        }
+        model = new CartesianChartModel();
+        model.addSeries(groupSessionSeries);
+        model.addSeries(ptSessionSeries);
+
     }
 
     public CartesianChartModel getModel() {
         if (model == null) {
-            createChart();
+            createSessionsChart();
         }
         return model;
     }
