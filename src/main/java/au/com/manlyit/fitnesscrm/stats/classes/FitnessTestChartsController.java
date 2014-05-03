1c56ba36f8e2b6195b8404b9f01bba26b6272052
david <david@david-h8-1210a>
2014-05-03 17:12:25 +1000

    REdid templates and reworked fitnessTestCharts crud

--- src/main/java/au/com/manlyit/fitnesscrm/stats/classes/FitnessTestChartsController.java
+++ src/main/java/au/com/manlyit/fitnesscrm/stats/classes/FitnessTestChartsController.java
@@ -6,30 +6,46 @@ import au.com.manlyit.fitnesscrm.stats.classes.util.PaginationHelper;
 import au.com.manlyit.fitnesscrm.stats.beans.FitnessTestChartsFacade;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
-import javax.ejb.EJB;
-import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ValueChangeEvent;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
 import javax.faces.model.SelectItem;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.primefaces.event.RowEditEvent;
+import org.primefaces.event.SelectEvent;
 
 @Named("fitnessTestChartsController")
 @SessionScoped
 public class FitnessTestChartsController implements Serializable {
 
     private FitnessTestCharts current;
+    private FitnessTestCharts selectedForDeletion;
     private DataModel items = null;
-    @EJB
+    @Inject
     private au.com.manlyit.fitnesscrm.stats.beans.FitnessTestChartsFacade ejbFacade;
+    @Inject
+    private au.com.manlyit.fitnesscrm.stats.beans.ConfigMapFacade configMapFacade;
     private PaginationHelper pagination;
     private int selectedItemIndex;
+    private List<FitnessTestCharts> filteredItems;
+    private FitnessTestCharts[] multiSelected;
 
     public FitnessTestChartsController() {
+    }
+
+    public static boolean isUserInRole(String roleName) {
+        boolean inRole = FacesContext.getCurrentInstance().getExternalContext().isUserInRole(roleName);
+        return inRole;
     }
 
     public FitnessTestCharts getSelected() {
@@ -40,13 +56,21 @@ public class FitnessTestChartsController implements Serializable {
         return current;
     }
 
+    public void setSelected(FitnessTestCharts selected) {
+        if (selected != null) {
+            current = selected;
+            selectedItemIndex = -1;
+        }
+
+    }
+
     private FitnessTestChartsFacade getFacade() {
         return ejbFacade;
     }
 
     public PaginationHelper getPagination() {
         if (pagination == null) {
-            pagination = new PaginationHelper(10) {
+            pagination = new PaginationHelper(1000000) {
 
                 @Override
                 public int getItemsCount() {
@@ -62,14 +86,42 @@ public class FitnessTestChartsController implements Serializable {
         return pagination;
     }
 
+    /**
+     * @return the filteredItems
+     */
+    public List<FitnessTestCharts> getFilteredItems() {
+        return filteredItems;
+    }
+
+    /**
+     * @param filteredItems the filteredItems to set
+     */
+    public void setFilteredItems(List<FitnessTestCharts> filteredItems) {
+        this.filteredItems = filteredItems;
+    }
+
+    /**
+     * @return the multiSelected
+     */
+    public FitnessTestCharts[] getMultiSelected() {
+        return multiSelected;
+    }
+
+    /**
+     * @param multiSelected the multiSelected to set
+     */
+    public void setMultiSelected(FitnessTestCharts[] multiSelected) {
+        this.multiSelected = multiSelected;
+    }
+
     public String prepareList() {
         recreateModel();
         return "List";
     }
 
     public String prepareView() {
-        current = (FitnessTestCharts) getItems().getRowData();
-        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
+        //current = (FitnessTestCharts)getItems().getRowData();
+        //selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
         return "View";
     }
 
@@ -81,37 +133,58 @@ public class FitnessTestChartsController implements Serializable {
 
     public String create() {
         try {
+            if (current.getId() == null) {
+                current.setId(0);
+            }
             getFacade().create(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle2").getString("FitnessTestChartsCreated"));
+            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("FitnessTestChartsCreated"));
             return prepareCreate();
         } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle2").getString("PersistenceErrorOccured"));
+            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
             return null;
         }
     }
 
+    public void createDialogue(ActionEvent actionEvent) {
+        try {
+            current.setId(0);
+            getFacade().create(current);
+            recreateModel();
+            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("FitnessTestChartsCreated"));
+        } catch (Exception e) {
+            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+        }
+
+    }
+
     public String prepareEdit() {
-        current = (FitnessTestCharts) getItems().getRowData();
-        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
+        //current = (FitnessTestCharts)getItems().getRowData();
+        //selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
         return "Edit";
+    }
+
+    public void selectOneMenuValueChangeListener(ValueChangeEvent vce) {
+        Object o = vce.getNewValue();
+    }
+
+    public void selectManyMenuValueChangeListener(ValueChangeEvent vce) {
+        Object o = vce.getNewValue();
     }
 
     public String update() {
         try {
             getFacade().edit(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle2").getString("FitnessTestChartsUpdated"));
+            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("FitnessTestChartsUpdated"));
             return "View";
         } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle2").getString("PersistenceErrorOccured"));
+            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
             return null;
         }
     }
 
     public String destroy() {
-        current = (FitnessTestCharts) getItems().getRowData();
-        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
+       
         performDestroy();
-        recreatePagination();
         recreateModel();
         return "List";
     }
@@ -129,12 +202,25 @@ public class FitnessTestChartsController implements Serializable {
         }
     }
 
+    public FitnessTestCharts getSelectedForDeletion() {
+        return selectedForDeletion;
+    }
+
+    public void setSelectedForDeletion(FitnessTestCharts selectedForDeletion) {
+        this.selectedForDeletion = selectedForDeletion;
+        current = selectedForDeletion;
+
+        performDestroy();
+        recreateModel();
+
+    }
+
     private void performDestroy() {
         try {
             getFacade().remove(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle2").getString("FitnessTestChartsDeleted"));
+            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("FitnessTestChartsDeleted"));
         } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle2").getString("PersistenceErrorOccured"));
+            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
         }
     }
 
@@ -162,16 +248,20 @@ public class FitnessTestChartsController implements Serializable {
 
     private void recreateModel() {
         items = null;
-    }
-
-    private void recreatePagination() {
-        pagination = null;
+        filteredItems = null;
     }
 
     public String next() {
         getPagination().nextPage();
         recreateModel();
         return "List";
+    }
+
+    public void handleDateSelect(SelectEvent event) {
+
+        Date date = (Date) event.getObject();
+
+        //Add facesmessage
     }
 
     public String previous() {
@@ -188,21 +278,27 @@ public class FitnessTestChartsController implements Serializable {
         return JsfUtil.getSelectItems(ejbFacade.findAll(), true);
     }
 
-    public FitnessTestCharts getFitnessTestCharts(java.lang.Integer id) {
-        return ejbFacade.find(id);
+    public void onEdit(RowEditEvent event) {
+        FitnessTestCharts cm = (FitnessTestCharts) event.getObject();
+        getFacade().edit(cm);
+        recreateModel();
+        JsfUtil.addSuccessMessage("Row Edit Successful");
+    }
+
+    public void onCancel(RowEditEvent event) {
+        JsfUtil.addErrorMessage("Row Edit Cancelled");
     }
 
     @FacesConverter(forClass = FitnessTestCharts.class)
     public static class FitnessTestChartsControllerConverter implements Converter {
 
-        @Override
         public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
             if (value == null || value.length() == 0) {
                 return null;
             }
             FitnessTestChartsController controller = (FitnessTestChartsController) facesContext.getApplication().getELResolver().
                     getValue(facesContext.getELContext(), null, "fitnessTestChartsController");
-            return controller.getFitnessTestCharts(getKey(value));
+            return controller.ejbFacade.find(getKey(value));
         }
 
         java.lang.Integer getKey(String value) {
@@ -212,12 +308,11 @@ public class FitnessTestChartsController implements Serializable {
         }
 
         String getStringKey(java.lang.Integer value) {
-            StringBuilder sb = new StringBuilder();
+            StringBuffer sb = new StringBuffer();
             sb.append(value);
             return sb.toString();
         }
 
-        @Override
         public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
             if (object == null) {
                 return null;
@@ -226,7 +321,7 @@ public class FitnessTestChartsController implements Serializable {
                 FitnessTestCharts o = (FitnessTestCharts) object;
                 return getStringKey(o.getId());
             } else {
-                throw new IllegalArgumentException("object " + object + " is of type " + object.getClass().getName() + "; expected type: " + FitnessTestCharts.class.getName());
+                throw new IllegalArgumentException("object " + object + " is of type " + object.getClass().getName() + "; expected type: " + FitnessTestChartsController.class.getName());
             }
         }
 
