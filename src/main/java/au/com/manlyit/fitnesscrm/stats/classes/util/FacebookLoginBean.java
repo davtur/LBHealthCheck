a12c63fc3569e0807eca8afa11097c57120230d4
david <david@david-h8-1210a>
2014-08-16 17:47:51 +1000

    bug fixes:
    fixed facebook login when session exists.
    
    fixed hours and minutes on log sessions

--- src/main/java/au/com/manlyit/fitnesscrm/stats/classes/util/FacebookLoginBean.java
+++ src/main/java/au/com/manlyit/fitnesscrm/stats/classes/util/FacebookLoginBean.java
@@ -5,12 +5,17 @@
  */
 package au.com.manlyit.fitnesscrm.stats.classes.util;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
 
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -21,19 +26,45 @@ import javax.servlet.http.HttpSession;
 @SessionScoped
 public class FacebookLoginBean implements Serializable {
 
+    private static final Logger logger = Logger.getLogger(FacebookLoginBean.class.getName());
     private static final long serialVersionUID = -1611162265998907599L;
     @Inject
     private au.com.manlyit.fitnesscrm.stats.beans.ConfigMapFacade configMapFacade;
 
     public String getFacebookUrlAuth() {
+        String returnValue ="";
+        FacesContext context = FacesContext.getCurrentInstance();
+        ExternalContext ec = context.getExternalContext();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+
         HttpSession session
-                = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
-        String sessionId = session.getId();
-        String appId = configMapFacade.getConfig("facebook.app.id");//"247417342102284";
-        String redirectUrl = configMapFacade.getConfig("facebook.redirect.url");//"http://localhost:8080/FitnessStats/index.sec";
-        String returnValue = configMapFacade.getConfig("facebook.app.oauth.url") + "client_id="
-                + appId + "&redirect_uri=" + redirectUrl
-                + "&scope=email,user_birthday&state=" + sessionId;
+                = (HttpSession) ec.getSession(false);
+        String userName = (String) session.getAttribute("FACEBOOK_USER");
+        if (userName != null) {
+           
+                String isMobile = (String) session.getAttribute("MOBILE_DEVICE");
+                String landingPage;
+                if (isMobile.contains("TRUE")) {                    
+                    landingPage = configMapFacade.getConfig("facebook.redirect.mobilelandingpage");
+                } else {
+                    landingPage = configMapFacade.getConfig("facebook.redirect.landingpage");
+                }
+                String message ="The user " + userName + " is already logged in. Redirecting to the landing Page:" + landingPage;
+                logger.log(Level.INFO, message);
+                 try {
+                ec.redirect(request.getContextPath() + landingPage);                
+            } catch (IOException ex) {
+                Logger.getLogger(FacebookLoginBean.class.getName()).log(Level.SEVERE, "Redirecting to Landing Page:", ex);
+            }
+        } else {
+            String sessionId = session.getId();
+            String appId = configMapFacade.getConfig("facebook.app.id");//"247417342102284";
+            String redirectUrl = configMapFacade.getConfig("facebook.redirect.url");//"http://localhost:8080/FitnessStats/index.sec";
+            returnValue = configMapFacade.getConfig("facebook.app.oauth.url") + "client_id="
+                    + appId + "&redirect_uri=" + redirectUrl
+                    + "&scope=email,user_birthday&state=" + sessionId;
+            
+        }
         return returnValue;
     }
 
