6c42804e176c13e04dd4fe7e8caa6d6eed5a5b13
david <david@david-h8-1210a>
2015-07-12 12:55:03 +1000

    commit before starting work on health questionaire survey
    Version 2.3.7

--- sql-changes.sql
+++ sql-changes.sql
@@ -1,2 +1,16 @@
 
 
+
+
+ALTER TABLE `fitnessStats`.`notes` ADD COLUMN `created_by` INT(11) NOT NULL DEFAULT 3 AFTER `deleted`;
+ALTER TABLE `fitnessStats`.`notes` 
+ADD INDEX `fk_notes_1_idx` (`created_by` ASC);
+ALTER TABLE `fitnessStats`.`notes` 
+ADD CONSTRAINT `fk_notes_1`
+  FOREIGN KEY (`created_by`)
+  REFERENCES `fitnessStats`.`customers` (`id`)
+  ON DELETE RESTRICT
+  ON UPDATE RESTRICT;
+
+
+
