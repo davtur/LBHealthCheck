3204b7994dfa5bd003df16e237b982471d4ba0a4
david <david@david-h8-1210a>
2015-07-18 16:31:59 +1000

    add todo list table and webpages. bump[ed atmosphere minot verion to 2.3.3 from 2.3.1
    Version 2.3.9

---
+++ src/main/java/au/com/manlyit/fitnesscrm/stats/db/ToDoList.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package au.com.manlyit.fitnesscrm.stats.db;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author david
+ */
+@Entity
+@Table(name = "toDoList")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "ToDoList.findAll", query = "SELECT t FROM ToDoList t"),
+    @NamedQuery(name = "ToDoList.findById", query = "SELECT t FROM ToDoList t WHERE t.id = :id"),
+    @NamedQuery(name = "ToDoList.findByTask", query = "SELECT t FROM ToDoList t WHERE t.task = :task"),
+    @NamedQuery(name = "ToDoList.findByDone", query = "SELECT t FROM ToDoList t WHERE t.done = :done")})
+public class ToDoList implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "task")
+    private String task;
+    @Basic(optional = false)
+    @NotNull
+    @Lob
+    @Size(min = 1, max = 65535)
+    @Column(name = "description")
+    private String description;
+    @Column(name = "done")
+    private Boolean done;
+
+    public ToDoList() {
+    }
+
+    public ToDoList(Integer id) {
+        this.id = id;
+    }
+
+    public ToDoList(Integer id, String task, String description) {
+        this.id = id;
+        this.task = task;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ToDoList)) {
+            return false;
+        }
+        ToDoList other = (ToDoList) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "au.com.manlyit.fitnesscrm.stats.db.ToDoList[ id=" + id + " ]";
+    }
+    
+}
